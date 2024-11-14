327ad77de775def636a41ceb46f6c36ddacebba4
Antoine Wurtz <antoine.wurtz@crowdaa.com>
2024-11-14 12:30:45 +0100

    add back button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PDFPreview from './PDFPreview';
 
 const Form: React.FC = () => {
@@ -17,8 +18,9 @@ const Form: React.FC = () => {
     return (
         <div className="bg-white p-6 rounded shadow-md w-full max-w-md">
             <h2 className="text-2xl font-bold mb-4">Créer une Lettre</h2>
+            <p className="text-md font-thin mb-6">Saisissez les informations qui apparaitront sur le document généré.</p>
             <div className="mb-4">
-                <label className="block font-bold mb-2">Société</label>
+                <label className="block font-bold mb-2">Destinataire</label>
                 <input
                     type="text"
                     className="w-full border border-gray-300 p-2 rounded"
@@ -44,9 +46,19 @@ const Form: React.FC = () => {
                     onChange={(e) => setContent(e.target.value)}
                 ></textarea>
             </div>
-            <button onClick={handleGeneratePDF} className="w-full bg-blue-600 text-white p-2 rounded">
-                Générer le PDF
-            </button>
+
+            <div className="flex justify-between mt-6">
+                <Link
+                    to="/"
+                    className="bg-red-500 text-white p-2 rounded w-48 hover:bg-red-600 text-center m-2"
+                >
+                    Retour
+                </Link>
+
+                <button onClick={handleGeneratePDF} className="bg-blue-500 text-white p-2 rounded w-48 hover:bg-blue-600 text-center m-2">
+                    Générer le PDF
+                </button>
+            </div>
         </div>
     );
 };
